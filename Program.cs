b9f1e1b31444e5829f6da28aca29b54506b16235
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2019-03-21 16:02:12 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace askforpassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Password: ");
+            string passwordUser = Console.ReadLine();
+            string passwordCorrect = "dog3411";
+            if (passwordUser == passwordCorrect)
+            {
+                Console.WriteLine("Welcome to the system");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password! Go home ...\"..");
+            }
+            Console.ReadLine();
+
+
+            //If it is the correct password, it should be displayed "Welcome to the system".
+            //If there is a wrong password, it should show "Wrong password! Go home ..."..
+        }
+    }
+}
